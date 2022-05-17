e1f3ac67c9623240b658aeefe7b307cfdc57902f
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-05-18 00:01:08 +0200

    fixat med POST och unfinished DELETE

--- server.js
+++ server.js
@@ -4,20 +4,17 @@ const fs = require("fs");
 
 port = 5000;
 
-
+    let todoPost = { 
+    todo: 'Diskdfsdfsda',
+   
+};
 
 const app = http.createServer((req, res) => {
 
     const items = req.url.split("/") 
 
-    if (req.method === "GET" && items[1] === "todos") { 
-
-        
-      
-      
-      
-      
-         fs.readFile('todos.json', (err, data) => {
+    if (req.method === "GET" && items[1] === "todos") {   
+         fs.readFile("todos.json", (err, data) => {
             if (err) throw err;
             let todos = JSON.parse(data);
             res.statusCode = 200;
@@ -25,7 +22,58 @@ const app = http.createServer((req, res) => {
         res.end(JSON.stringify(todos));
         });
         
-      }
+     }   else if (req.method === "POST"){
+        
+        fs.readFile("todos.json", (error, data) => {
+            if (error) {
+              console.log(error);
+              return;
+            }
+            
+            fs.writeFile("todos.json", JSON.stringify(todoPost, null, 2), (err) => {
+              if (err) {
+                console.log('Failed to write updated data to file');
+                return;
+              }
+              console.log('Updated file successfully');
+            });
+          });
+
+
+      /*  fs.readFile('todos.json', function (err, data) {
+            let json = JSON.parse(data);
+            json.push(todoPost);    
+            let todoPost2 = JSON.stringify(todoPost);
+            fs.writeFile("todos.json", todoPost2, function(err){
+              if (err) throw err;
+              console.log('The "data to append" was appended to file!');
+            });
+        }) */
+
+
+      /*  let json = JSON.stringify(todoPost, null, 2);
+
+        fs.appendFile('todos.json', json, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+        }); */
+        res.statusCode = 201;
+        res.end();
+    } else if (req.method === "DELETE") {
+        fs.unlink("todos.json",function(err, data) {
+            if (err) throw err;
+            console.log(data);
+        }); 
+          console.log("\nFile Deleted!\n");
+        
+        res.statusCode = 204;
+        res.end();
+      
+    }
+      else {
+        res.statusCode = 404;
+        res.end();
+    }
 
 })
 
