2e9fbeebecc7c490359342aba3587bac613ca3b0
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-05-17 15:26:59 +0200

    adderade GET

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const http = require("http");
+const fs = require("fs");
+
 
 port = 5000;
 
+
+
 const app = http.createServer((req, res) => {
+
+    if (req.method === "GET") { 
+
+        
+      
+      
+      
+      
+         fs.readFile('todos.json', (err, data) => {
+            if (err) throw err;
+            let todos = JSON.parse(data);
+            res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(todos));
+        });
+        
+      }
 
 })
 
