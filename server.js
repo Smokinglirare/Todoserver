0d631d68944eeb5cddf0a3d416f2b6523e57f034
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-05-20 16:55:06 +0200

    DELETE och PUT klar

--- server.js
+++ server.js
@@ -2,76 +2,97 @@ const http = require("http");
 const fs = require("fs");
 
 
+
 port = 5000;
 
 
 const max = 10000000;
 
-
+/*
     let todoPost = { 
     id: Math.floor(Math.random() * max),
     todo: 'Diskdfsdfsda',
    
-};
+}; */
+let todos = [];
 
+fs.readFile("todos.json", (err, data) => {
+    if (err) throw err;
+    todos = JSON.parse(data);
+})
+        
 const app = http.createServer((req, res) => {
 
-    const items = req.url.split("/") 
- //   console.log(items);
-   // console.log(items[2]);
-    
+    const items = req.url.split("/")
+    //   console.log(items);
+    //console.log(items[2]);
+    //console.log(items[1]);
 
-    if (req.method === "GET" && items[1] === "todos" && items.length === 2) {   
-         fs.readFile("todos.json", (err, data) => {
-            if (err) throw err;
-            let todos = JSON.parse(data);
+
+    if (req.method === "GET" && items[1] === "todos" && items.length === 2) {
             res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(todos));
-        });
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(todos));
         
-        
-     }  else if (req.method === "GET" && items.length === 3){
-        fs.readFile("todos.json", (err, data) => {
-            if (err) throw err;
-            let todos = JSON.parse(data);
+
+
+    } else if (req.method === "GET" && items.length === 3) {
             res.statusCode = 200;
             todoIndex = parseInt(items[2]);
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(todos[todoIndex]));
-        
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(todos[todoIndex]));
 
-     })  
-    } else if (req.method === "POST"){
+
         
-     
-        fs.readFile('todos.json', function (err, data) {
-            let json = JSON.parse(data);
-            console.log(json);
-            json.push(todoPost);    
-            let todoPost2 = JSON.stringify(json);
-            fs.writeFile("todos.json", todoPost2, function(err){
-              if (err) throw err;
-              console.log('data skriven');
+    } else if (req.method === "POST") {
+            let todoPost = {
+                id: Math.floor(Math.random() * max),
+                todo: 'Diskdfsdfsda',
+
+            };
+            todos.push(todoPost);
+            let todoPost2 = JSON.stringify(todos);
+            fs.writeFile("todos.json", todoPost2, function (err) {
+                if (err) throw err;
+                console.log('data skriven');
             });
-        }) 
         res.statusCode = 201;
         res.end();
-    
-      
-    } else if (req.method === "DELETE" ) {
-       res.statusCode = 204;
-       req.on("data", (chunk) => {
-           const data = JSON.parse(chunk);
-           console.log(data, "test id")
-       })
-       
-      }
-      else {
+
+
+    } else if (req.method === "DELETE" && items[1] === "todos" && items.length === 3) {
+            const todoID = parseInt(items[2]);
+            todos = todos.filter(todo => parseInt(todo.id) !== todoID);
+            fs.writeFile("todos.json", JSON.stringify(todos), (err,) => {
+                if (err) throw err;
+                res.statusCode = 200;
+                res.end();
+            });
+
+        
+    }
+    else if (req.method === "PUT" && items[1] === "todos" && items.length === 3) {
+        // PUT ersätter hela objektet
+            const todoID = parseInt(items[2]);
+            req.on("data", (chunk) => {
+                data = JSON.parse(chunk);
+                todos = todos.map(todo => { if (todo.id === todoID) { todo.todo = data.todo } });
+              })
+            
+
+            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+                if (err) throw err;
+                res.statusCode = 200;
+                res.end();
+            });
+        
+    }
+
+    else {
         res.statusCode = 404;
         res.end();
     }
-     })
+})
 
 app.listen(port, () => {
     console.log(`Nu körs servern på port ${port}`)
